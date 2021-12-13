3083755563b1c2e4c24169aab5a328f67db882c4
georgemoralis <giorgosmrls@gmail.com>
2021-12-13 14:31:59 +0200

    more drivers refactoring

--- emulator/src/main/java/arcadeflex/v037b7/drivers/goindol.java
+++ emulator/src/main/java/arcadeflex/v037b7/drivers/goindol.java
@@ -3,7 +3,7 @@
  * ported to v0.36
  *
  */
-package gr.codebb.arcadeflex.v037b7.drivers;
+package arcadeflex.v037b7.drivers;
 
 import static gr.codebb.arcadeflex.v036.mame.driverH.*;
 import static gr.codebb.arcadeflex.v037b7.mame.memoryH.*;
